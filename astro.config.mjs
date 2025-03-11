956e338629ce21376144251794628bccd43ff6ad
Sloane-J <samueldorkeyjr@gmail.com>
2025-03-11 13:26:19 +0000

    starting all over again

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
+  site: 'https://myastro-blogg.netlify.app',
   integrations: [mdx(), svelte(), tailwind()],
   markdown: {
     shikiConfig: {
